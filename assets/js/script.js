89d52eeede4ba2b17d7d9fca61a3082accdd3778
Vlad <kv37419@mail.ru>
2022-10-15 11:11:20 +0300

    begin

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+let wrapper = document.querySelector('.wrapper');
+
+let mainSlider = new Swiper('.main', {
+    wrapperClass: 'main__wrapper',
+    slideClass: 'main__section',
+
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    parallax: true,
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+
+    mousewheel: {
+        sensitivity: .4,
+    },
+
+    watchOverflow: true,
+
+    speed: 800,
+
+    observer: true,
+
+    observeParents: true,
+
+    observeSlideChildren: true,
+
+    scrollbar: {
+        el: '.main__scroll',
+        dragClass: 'main__drag-scroll',
+        draggable: false,
+    },
+
+    init: false,
+
+    on: {
+        init: function () {
+            setScrollType();
+            wrapper.classList.add('_loaded');
+        },
+        resize: function () {
+            setScrollType();
+        },
+    },
+});
+
+function setScrollType() {
+    if (wrapper.classList.contains('_free')) {
+        wrapper.classList.remove('_free');
+        mainSlider.params.freeMode.enabled = false;
+    }
+
+    for (let i = 0; i < mainSlider.slides.length; i++) {
+        const mainSlide = mainSlider.slides[i];
+        const mainSlideContent = mainSlide.querySelector('.section__content');
+
+        if (mainSlideContent) {
+            const mainSlideContentHeight = mainSlideContent.offsetHeight;
+            if (mainSlideContentHeight > window.innerHeight) {
+                wrapper.classList.add('_free');
+                mainSlider.params.freeMode.enabled = true;
+                break;
+            }
+        }
+    }
+
+    
+
+}
+
+mainSlider.init();
