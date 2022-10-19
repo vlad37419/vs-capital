d1a1f3229fee63387a08c44f2f0b7ac952b47621
Vlad <kv37419@mail.ru>
2022-10-19 12:44:42 +0300

    add more animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,5 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
     let wrapper = document.querySelector('.wrapper');
-
-    // let mainSlider = new Swiper('.main', {
-    //     wrapperClass: 'main__wrapper',
-    //     slideClass: 'main__section',
-
-    //     preventInteractionOnTransition: true,
-    //     direction: 'vertical',
-    //     slidesPerView: 'auto',
-    //     parallax: true,
-    //     resistance: false,
-    //     touchMoveStopPropagation: false,
-    //     touchReleaseOnEdges: true,
-
-    //     keyboard: {
-    //         enabled: true,
-    //         onlyInViewport: true,
-    //         pageUpDown: true,
-    //     },
-
-    //     mousewheel: {
-    //         sensitivity: .8,
-    //     },
-
-    //     watchOverflow: true,
-
-    //     speed: 800,
-
-    //     observer: true,
-
-    //     observeParents: true,
-
-    //     observeSlideChildren: true,
-
-    //     scrollbar: {
-    //         el: '.main__scroll',
-    //         dragClass: 'main__drag-scroll',
-    //         draggable: false,
-    //     },
-
-    //     init: false,
-
-    //     on: {
-    //         init: function () {
-    //             setScrollType();
-    //             wrapper.classList.add('_loaded');
-    //         },
-    //         resize: function () {
-    //             setScrollType();
-    //         },
-    //         slideChange: function () {
-    //             animOnScroll();
-    //         },
-    //     },
-    // });
-
-    // function setScrollType() {
-    //     if (wrapper.classList.contains('_free')) {
-    //         wrapper.classList.remove('_free');
-    //         mainSlider.params.freeMode.enabled = false;
-    //         mainSlider.params.preventInteractionOnTransition = true;
-    //         mainSlider.params.speed = 800;
-    //         mainSlider.params.resistanceRatio = 0.85;
-    //     }
-
-    //     for (let i = 0; i < mainSlider.slides.length; i++) {
-    //         const mainSlide = mainSlider.slides[i];
-    //         const mainSlideContent = mainSlide.querySelector('.section__content');
-
-    //         if (mainSlideContent) {
-    //             const mainSlideContentHeight = mainSlideContent.offsetHeight;
-    //             if (mainSlideContentHeight > window.innerHeight) {
-    //                 wrapper.classList.add('_free');
-    //                 mainSlider.params.freeMode.enabled = true;
-    //                 mainSlider.params.preventInteractionOnTransition = false;
-    //                 mainSlider.params.speed = 100;
-    //                 mainSlider.params.resistanceRatio = 0;
-    //                 break;
-    //             }
-    //         }
-    //     }
-    // }
 
     // Анимация при скролле
     const animItems = document.querySelectorAll('.anim-item');
@@ -118,9 +35,30 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    const mainSection = document.querySelectorAll('.main__section');
+
+    function addCssTopForStickyBlock () {
+        console.log('test');
+        if (mainSection.length > 0) {
+            for (let i = 0; i < mainSection.length; i += 1) {
+                let section = mainSection[i];
+                let sectionHeight = section.offsetHeight;
+                let windowHeight = document.documentElement.clientHeight;
+        
+                if (sectionHeight > windowHeight) {
+                    section.style.top = (windowHeight - sectionHeight) + 'px';
+                } else {
+                    section.style.top = 0 + 'px';
+                }
+            }
+        }
+    }
+
+    window.onresize = function() {
+        addCssTopForStickyBlock();
+    };
+    
     animOnScroll();
 
-    // mainSlider.init();
-
-    animOnScroll();
+    setTimeout(addCssTopForStickyBlock(), 500);
 });
